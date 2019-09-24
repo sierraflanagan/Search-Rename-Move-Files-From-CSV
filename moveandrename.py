068171659276abfa7618371febe8e4e828fa5b54
Sierra <40186374+sierraflanagan@users.noreply.github.com>
2019-09-23 22:34:05 -0400

    Update moveandrename.py
    Adding comments

--- moveandrename.py
+++ moveandrename.py
@@ -3,12 +3,16 @@ import shutil
 import os
 
 # insert csv file 
+# Load CSV with columns of old filenames and new filenames as pandas dataframe
 df = pd.read_csv('/Users/sierraflanagan/Documents/filemovertest/textfilemover.csv') 
+# Get amount of files, to be used to iterate
 total_indeces = df['old_filename'].size
+# Broadest directory of old filenames, code will search subfolders
 oldfolder = '/Users/sierraflanagan/Documents/filemovertest/'
-old_col = 'old_filename' #column heading of old filenames
-new_col = 'new_filename' #column heading of new filenames
+old_col = 'old_filename' #column heading of old filenames - Changes from .csv to .csv
+new_col = 'new_filename' #column heading of new filenames - Changes from .csv to .csv
 
+# Main method searches folders for old filename, and renames ans copies files to new directory
 def main():
     for i in range(total_indeces):
         oldName = df.at[i, old_col]
@@ -18,11 +22,13 @@ def main():
         new_ext = create_new_filepath(newName)
         shutil.move(old_ext, new_ext)
 
+# Finds filepath given a file name
 def find_filepath(name, path):
     for root, dirs, files in os.walk(path):
         if name in files:
             return os.path.join(root,name)
 
+# Generates a new filepath to copy and rename files to
 def create_new_filepath(newname):
     #The new folder where the renamed files will be moved
     newfolder = '/Users/sierraflanagan/Documents/filemovertest/'
